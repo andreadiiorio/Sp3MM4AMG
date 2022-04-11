42a9a61c6b6c88878b010fb38d9eb11b535190d8
Andrea Di Iorio <andrea.diiorio.96@gmail.com>
2022-04-11 11:00:02 +0200

    little mod to the thesis ;) delivered yesterday!!!!!

--- doc/buildTex.sh
+++ doc/buildTex.sh
@@ -1,2 +1,9 @@
-rm *aux 
-~/DATA/SW/latex/2021/bin/x86_64-linux/pdflatex  main.tex && ~/DATA/SW/latex/2021/bin/x86_64-linux/bibtex main ; ~/DATA/SW/latex/2021/bin/x86_64-linux/pdflatex  main && ~/DATA/SW/latex/2021/bin/x86_64-linux/pdflatex  main.tex
+pdflatex="~/DATA/SW/latex/2021/bin/x86_64-linux/pdflatex -jobname=out " #-output-directory=build " #-halt-on-error"
+bibtex="~/DATA/SW/latex/2021/bin/x86_64-linux/bibtex"
+#rm out* *aux
+eval $pdflatex  $1
+if [[ $? != 0 ]];then exit 1;fi
+if [[ -z $NO_REFERENCES ]];then		#rebuilds to make reference available
+	eval $bibtex out; eval $pdflatex  $1&& eval $pdflatex  $1
+fi
+if [[ $? ]];then evince "out.pdf";fi
--- doc/buildTexChi.sh
+++ doc/buildTexChi.sh
@@ -1,14 +1,18 @@
 trgtCh="Chapters/ChIntro"
-trgtChOutName="ChIntro"
-if [[ $TRGTCH ]];then			trgtCh="$TRGTCH";fi
+trgtChOutName="ch"
+if [[ $1 ]];then				trgtCh="$1";fi
 if [[ $TRGTCHOUTNAME ]];then	trgtChOutName="$TRGTCHOUTNAME";fi
 
-pdflatex="~/DATA/SW/latex/2021/bin/x86_64-linux/pdflatex"
+mkdir -p build
+pdflatex="~/DATA/SW/latex/2021/bin/x86_64-linux/pdflatex -output-directory=build " #-halt-on-error"
 bibtex="~/DATA/SW/latex/2021/bin/x86_64-linux/bibtex"
 trgtChLimit=" -jobname=$trgtChOutName \"\includeonly{$trgtCh}\input{main.tex}\""
 ################################################################################
-eval $pdflatex "$trgtChLimit" main.tex && \
-eval $bibtex $trgtChOutName; \
-eval $pdflatex "$trgtChLimit" main.tex && \
-eval $pdflatex "$trgtChLimit" main.tex &&\
- evince "$trgtChOutName".pdf
+eval $pdflatex "$trgtChLimit" main.tex
+if [[ $? != 0 ]];then exit 1;fi
+if [[ -z $NO_REFERENCES ]];then		#rebuilds to make reference available
+	eval $bibtex $trgtChOutName; 
+	eval $pdflatex "$trgtChLimit" main.tex && \
+	eval $pdflatex "$trgtChLimit" main.tex
+fi
+if [[ $? ]];then evince "build/$trgtChOutName".pdf;fi
