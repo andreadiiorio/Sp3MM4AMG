5b45e841c6a16ca419ae211af877ae76e9a624a1
Andrea Di Iorio <andrea.diiorio.96@gmail.com>
2023-02-05 23:40:33 +0100

    wrapped source matrix and test script into Makefile testAll

--- code/scripts/testAllMatrixGroups.sh
+++ code/scripts/testAllMatrixGroups.sh
@@ -1,6 +1,6 @@
 set -e -o pipefail
 trgtbin="test_Sp3MM.o"
-if [ $1 ];then trgtbin="$1";fi
+if [[ $1 ]];then trgtbin="$1";fi
 export TRGTBIN="$trgtbin"
 #cat matrixGroups.list | xargs -L 1 sh -ec 'echo "## $0 ${@}"; $(realpath $TRGTBIN ) $0 ${@}' #TODO no error termination
 while read line; do
